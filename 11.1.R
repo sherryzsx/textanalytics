e243af206700fc9981f5acada8345ac3ad277af2
sherryzsx <shuxiaoz@usc.edu>
2017-11-01 21:13:55 -0700

    final version of the inclass work

--- 11.1.R
+++ 11.1.R
@@ -30,10 +30,27 @@ input <- c("abc","defg")
 str_sub(input,c(2,3))
 
 #7
+emails <- readRDS("email.rds")
+emails
+emails[1]
+cat(emails[1])
 
+a = str_locate(string = emails[1],pattern = "\n\n")
 
+#8.
+metadata1 = str_sub(string = emails[1],end = a[1] )
+body1 = str_sub(string = emails[1],start = a[2] )
 
+cat(metadata1)
+cat(body1)
 
+#9
+str_split(metadata1,"\n")
+
+#10
+breaks = str_locate(emails,pattern = "\n\n")
+metadata = str_sub(emails,end = breaks[,1]) # nothing before the , it will be whatever number email it is
+body = str_sub(emails,start = breaks[,2])
 
 
 
